8435f55d22c85aa638976f91e3b55510019c6233
LukeG1117 <luke.geyer@utahtech.edu>
2026-02-24 16:08:23 -0700

    added a button to link to the simulator

--- gravity_sim/src/App.tsx
+++ gravity_sim/src/App.tsx
@@ -1,9 +1,8 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom'; // 1. Import Link
 import About from './pages/About';
 import Scene from './pages/Scene';
 import GridTest from './pages/GridTest';
-
 
 function App() {
   return (
@@ -21,7 +20,19 @@ function App() {
 
 // Simple Home Component for demo
 function Home() {
-  return <div className='p-10 text-2xl'>Welcome Home!</div>;
+  return (
+    <div className='p-10 flex flex-col items-start gap-6'>
+      <div className='text-2xl'>Welcome Home!</div>
+      
+      {/* 2. Add the Link styled as a button */}
+      <Link 
+        to="/scene" 
+        className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-6 rounded shadow transition-colors"
+      >
+        Launch Gravity Simulator
+      </Link>
+    </div>
+  );
 }
 
 export default App;
